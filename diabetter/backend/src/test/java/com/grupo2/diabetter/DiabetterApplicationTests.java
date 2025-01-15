194efd516e1244fa838de2de437c1ae1f162a5a1
Geraldo JR <geraldo.sobreira.junior@ccc.ufcg.edu.br>
2025-01-14 21:54:29 -0300

    Creating backend project

---
+++ diabetter/backend/src/test/java/com/grupo2/diabetter/DiabetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.diabetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
