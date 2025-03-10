8739224580e7b59d25287017014823e92abfb19e
Orlandovpjunior <orlando.virginio.penha.junior@ccc.ufcg.edu.br>
2025-03-09 22:25:35 -0300

    chore: Adiciona prime React

--- diabetter/src/app/layout.tsx
+++ diabetter/src/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/NavBar";
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
