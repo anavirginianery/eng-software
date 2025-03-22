ed66af40438deae7b35154c441eb320c022a53c3
Axelvazslima <axelvaz45@gmail.com>
2025-03-22 14:42:18 -0300

    dashboard

--- diabetter/src/app/(user)/dashboard/page.tsx
+++ diabetter/src/app/(user)/dashboard/page.tsx
@@ -52,22 +52,6 @@
     };
 
     return (
-      <div className="flex h-screen">
-        <aside className="w-64 bg-white shadow-md p-6 flex flex-col">
-          <div className="flex flex-col items-center mb-6">
-            <div className="w-20 h-20 bg-gray-300 rounded-full mb-2"></div>
-            <span className="text-lg font-bold">João Gomes</span>
-          </div>
-          <nav className="flex flex-col space-y-4">
-            <Button label="Home" className="p-button-text" />
-            <Button label="Perfil" className="p-button-text p-button-primary" />
-            <Button label="Medições" className="p-button-text" />
-            <Button label="Dashboard" className="p-button-text" />
-          </nav>
-          <div className="mt-auto">
-            <Button label="Sair" className="p-button-text text-right" />
-          </div>
-        </aside>
         <main className="flex-col p-6 bg-gray-100 flex justify-center items-center w-full min-h-screen max-h-full">
         <div className='flex items-center justify-around w-full p-2'>
             <div className='flex flex-col'>
@@ -97,6 +81,5 @@
           <LineChart data={data} options={options} />
         </div>
         </main>
-      </div>
     );
   }
